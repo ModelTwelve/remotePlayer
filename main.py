ee449a9d38eb87eefe7e2c75defddf0a08d6fda6
ModelTwelve <github@vinacs.com>
2019-03-16 11:33:45 -0400

    Beginning to get a working player

--- main.py
+++ main.py
@@ -6,25 +6,24 @@ from buttonTestServer import ButtonTestServer
 import socket
 import asyncore
 
-def startButtonServer():
-    buttonServer = ButtonTestServer('localhost', 8080)
+
+def startButtonServer(rc):
+    buttonServer = ButtonTestServer('localhost', 8080, rc)
     asyncore.loop(timeout=1)
     print("done")
 
+def doWhenDone():
+    print("done playing")
+
 def main():
-    buttonServerThread = Thread(target = startButtonServer)
+    rc = RemoteControl('/home/kenny/Desktop/mp3',doWhenDone)
+    buttonServerThread = Thread(target = startButtonServer, args=(rc,))
     buttonServerThread.start()
-
-    rc = RemoteControl()
-    rc.Play('/home/kenny/Downloads/SampleAudio_0.7mb.mp3')
-    sleep(5)
-    rc.Play('/home/kenny/Downloads/SampleAudio_0.7mb.mp3')
-    sleep(5)
-    rc.Stop()
-
-    asyncore.close_all()
-    
-    buttonServerThread.join()
+    # Run forever!
+    buttonServerThread.join()    
+    #rc.Play('/home/kenny/Desktop/mp3/1980 - Back In Black/06 - Back In Black.mp3')
+    #sleep(5)
+    asyncore.close_all()    
     print("thread finished...exiting")
 
 if __name__ == "__main__":
--- remoteControl.py
+++ remoteControl.py
@@ -1,27 +1,67 @@
 import pexpect
-
+import os
 from threading import Thread
 from time import sleep
+from random import shuffle
 
 class RemoteControl():
 
     SPAWNPROCESS = '/usr/bin/mpg123 -R'    
     QUIT = "q\r"
 
-    def __init__(self):      
-        self.RemoteControlProcess = pexpect.spawn(self.SPAWNPROCESS)  
+    def __init__(self, MP3Dir, callback):        
+        self.Current=0
+        self.MP3Dir = MP3Dir
+        self.InitList()
+        self.callback = callback
+        self.RemoteControlProcess = pexpect.spawn(self.SPAWNPROCESS, timeout=None)  
 
         self.MonitorProcessThread = Thread(target=self.MonitorProcess)
         self.MonitorProcessThread.start()
 
-    def Play(self, filename):
-        cmd = "LOAD %s\r" % (filename)
+    def Play(self):
+        cmd = "LOAD %s\r" % (self.Playlist[self.Current])
         self.RemoteControlProcess.send(cmd) 
+    
+    def InitList(self): 
+        self.Playlist = []
+        for dirname, dirnames, filenames in os.walk(self.MP3Dir):        
+            for filename in filenames:
+                if filename.lower().endswith(".mp3"):
+                    self.Playlist.append(os.path.join(dirname, filename))
+        shuffle(self.Playlist)
+        self.Current=0    
+    
+    def Stop(self):
+        self.RemoteControlProcess.send("S\r") 
+
+    def Pause(self):
+        self.RemoteControlProcess.send("P\r") 
+
+    def Prev(self):
+        self.Stop()
+        self.Current -= 1
+        self.Play()      
+    
+    def Next(self):
+        self.Stop()
+        self.Current += 1
+        self.Play()        
 
     def MonitorProcess(self):
-        self.RemoteControlProcess.expect([pexpect.TIMEOUT, pexpect.EOF])           
+        ev=999
+        while ev > 1:
+            ev = self.RemoteControlProcess.expect(
+                [        
+                    pexpect.TIMEOUT, 
+                    pexpect.EOF,
+                    "@P 0",
+                ])
+            if ev == 2:
+                self.callback()
 
-    def Stop(self):
+
+    def Quit(self):
         self.RemoteControlProcess.send(self.QUIT)
         self.RemoteControlProcess.terminate(force=True)
 
