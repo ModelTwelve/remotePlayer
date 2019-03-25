b06fb3bd4f45f00591175490f394e7f9728d17c0
ModelTwelve <github@vinacs.com>
2019-03-25 17:42:08 -0400

    GPIO Quick Shutdown Button Combo

--- gpio.py
+++ gpio.py
@@ -1,31 +1,42 @@
 import RPi.GPIO as GPIO  
+import os
 
 class GPIOHandler():
     rc = None
+    _PLAY = 17
+    _PREV = 27
+    _NEXT = 18
+    _STOP = 22
 
     def __init__(self, rc1): 
         self.rc = rc1 
 
         GPIO.setmode(GPIO.BCM)
         # Play
-        GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+        GPIO.setup(self._PLAY, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
         # Prev
-        GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+        GPIO.setup(self._PREV, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
         # Next
-        GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+        GPIO.setup(self._NEXT, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
         # Stop/Init        
-        GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+        GPIO.setup(self._STOP, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
 
-        GPIO.add_event_detect(17, GPIO.FALLING, callback=self.CallBack_Play, bouncetime=300)  
-        GPIO.add_event_detect(27, GPIO.FALLING, callback=self.CallBack_Prev, bouncetime=300) 
-        GPIO.add_event_detect(18, GPIO.FALLING, callback=self.CallBack_Next, bouncetime=300)  
-        GPIO.add_event_detect(22, GPIO.FALLING, callback=self.CallBack_StopInit, bouncetime=300) 
+        GPIO.add_event_detect(self._PLAY, GPIO.FALLING, callback=self.CallBack_Play, bouncetime=300)  
+        GPIO.add_event_detect(self._PREV, GPIO.FALLING, callback=self.CallBack_Prev, bouncetime=300) 
+        GPIO.add_event_detect(self._NEXT, GPIO.FALLING, callback=self.CallBack_Next, bouncetime=300)  
+        GPIO.add_event_detect(self._STOP, GPIO.FALLING, callback=self.CallBack_StopInit, bouncetime=300) 
 
     def Finish(self):
         GPIO.cleanup()             
 
-    def CallBack_Play(self, channel):  
-        self.rc.Play()        
+    def CallBack_Play(self, channel):
+        # Check GPIO 22 Stop/Init
+        # If that button is also pushed then assume shutdown is requested instead
+        if GPIO.input(self._STOP) == GPIO.LOW:
+            os.system("shutdown now")      
+        else:
+            # Typical / expected
+            self.rc.Play()        
     def CallBack_Prev(self, channel):  
         self.rc.Prev()
     def CallBack_Next(self, channel):  
